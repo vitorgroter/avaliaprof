8a6ea03edc78273f8ebaf77ea1630246726ad60d
Vitor Meneguzzi Groterhorst <vitor023@gmail.com>
2011-10-04 11:39:06 -0300

    Inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avaliaprof::Application.config.secret_token = '6af5dc2e4424bdbcfebcac2c8819fba6113d2ec1f4b44bf8648eef66fc387a752161206e06903e6820a19d2ab29b0d91f2c06639a4045d476eda3b0d57e82a6b'
