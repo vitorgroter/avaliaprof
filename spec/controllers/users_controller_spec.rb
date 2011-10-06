9906fcc839a843ef3a0c9a68e05afe5845496fc9
Vitor Meneguzzi Groterhorst <vitor023@gmail.com>
2011-10-06 12:09:57 -0300

    Usuarios com senha e com pagina

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,6 +17,7 @@ describe UsersController do
     it "should find the right user" do
       get :show, :id => @user
       assigns(:user).should == @user
+    end
 
     it "should have the right title" do
       get :show, :id => @user
